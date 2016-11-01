c7922a160e4c58c9265d6969a0afda94f0d06a99
liujiajie <liujiajie@dianping.com>
2016-11-01 18:32:12 +0800

    【添加JJLeftViewController】

---
+++ JJSimpleRevealVCDemo/JJSimpleRevealVCDemo/JJLeftViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJLeftViewController.h
+//  JJSimpleRevealVCDemo
+//
+//  Created by 刘佳杰 on 16/11/1.
+//  Copyright © 2016年 刘佳杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJLeftViewController : UIViewController
+
+@end
